a7cdaddafdbd67ad21159e72633080ef6a00b5fe
javi <qualopec@gmail.com>
2017-11-16 16:19:46 +0100

    improved code to detect lines

--- sensor_camera.py
+++ sensor_camera.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 from sklearn.cluster import DBSCAN
+from scipy.cluster.vq import kmeans
 from scipy.signal import argrelextrema
 import time
 
@@ -36,6 +37,9 @@ def calculate_colors(im, lpf_size=10, lpf_passes=2, plot=None):
     # calculate local peaks
     t0 = time.time()
     maxs = argrelextrema(h[:, 1], np.greater, order=10)[0]
+    # after getting all the peaks in the histogram lets cluster all colors in two
+    # this is a way to remove background
+    maxs = kmeans(maxs.astype(np.float), 2)[0].astype(np.int)
     t1 = time.time()
     print ("local peaks time %f" % (t1 - t0))
     if plot:
@@ -79,7 +83,7 @@ def calculate_colors(im, lpf_size=10, lpf_passes=2, plot=None):
 
     return maximums, paletted
 
-def extract_lines_from_image(image, color_index, percent_discard=0.1, plot=None):
+def extract_lines_from_image(image, color_index, percent_discard=0.3, plot=None):
     """given an image and the color index to use calculate lines
     """
     x, y = np.where(image == color_index)
@@ -115,13 +119,13 @@ def extract_lines_from_image(image, color_index, percent_discard=0.1, plot=None)
           # compute varianze
           weight = float(x.shape[0])/image.size
           # discard small pieces
-          if weight > 0.0005:
+          if weight > 0.000005:
             xx = np.poly1d(pfd)
             err = (xx(x[:,0]) - x[:,1]).var()
             bbox = [x[:,0].min(), x[:,0].max(),x[:,1].min(), x[:,1].max()]
             # create line segment, like [x0, y0, x1, y1]
-            segment = (xx(bbox[0]), bbox[0], xx(bbox[1]), bbox[1])
-            pol.append((x, pfd, err, segment, weight))
+            segment = (xx(bbox[2]), bbox[2], xx(bbox[3]), bbox[3])
+            pol.append((x, pfd, err, segment, weight, bbox))
       except ValueError:
           print(x)
       t1 = time.time()
@@ -129,8 +133,8 @@ def extract_lines_from_image(image, color_index, percent_discard=0.1, plot=None)
 
 
     if plot:
-        xp = np.linspace(0, image.shape[0] , 100)
-        for c, po, err, segment, weight in pol:
+        for c, po, err, segment, weight, bbox in pol:
+           xp = np.linspace(bbox[0], bbox[1] , 100)
            p = np.poly1d(po)
            _ = plot.plot(c[:,1], c[:, 0], '.', p(xp), xp, '-')
     return pol
@@ -150,6 +154,7 @@ def extract_lines(im, plot=None):
     t1 = time.time()
     print ("calculate colors time %f" % (t1 - t0))
     #ax = None
+    lines = []
     ax = plot
     for i, x in enumerate(maximums[:,0]):
         #if plot and not ax:
@@ -159,6 +164,46 @@ def extract_lines(im, plot=None):
             ax.autoscale(False)
         t0 = time.time()
         pols = extract_lines_from_image(paletted, x, plot=ax)
+        for p in pols:
+            # the coordinate system is reversed, coordinate 0 is X and Y is 1, so reverse it so
+            # the following operations with this segments don't drive me crazy
+            # also normalize by the image size so next calcs can be done without the needing to know the bounds
+            lines.append(np.array([
+                [p[3][1]/im.shape[1], p[3][0]/im.shape[0]],
+                [p[3][3]/im.shape[1], p[3][2]/im.shape[0]]
+            ]))
         t1 = time.time()
         print ("extract lines from image time %f" % (t1 - t0))
+    return lines
+
+def calculate_direction(lines):
+    """
+    based on the lines extracted from the image this function calculates the real direccion the car needs to turn
+    The algortihm is really simple, get the lines (weighting by closeness and line lenght) and calculate the center point
+    higher values of Y is closer
+    """
+    
+    # calculate line weight based on distance
+    # find the total distance to weight based on distance
+    total_length = 0
+    for x in lines:
+        v = x[1] - x[0]
+        length = np.hypot(v[1], v[0])
+        total_length += length
+
+    # calculate centroid for lines
+    centroid = np.array([0.0, 0.0])
+    for x in lines:
+        # modulate centroid by lenght and distance to 
+        v = x[1] - x[0]
+        length = np.hypot(v[1], v[0])
+        line_centroid = 0.5 * (x[1] + x[0])
+        distance_weigth = 1.0 - 10**(-3*line_centroid[1])
+        length_weigth = length/total_length
+        centroid += line_centroid * distance_weigth * length_weigth
+    return centroid
+        
+
+
+
 
